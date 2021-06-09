fd89108ed3665588f3a7a05b03775c152be223b8
SheikSena <sheiksenareddy@gmail.com>
2021-06-09 22:01:41 +0530

    Initial commit

---
+++ src/test/java/com/learning/hystrixdashboard/HystrixDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.hystrixdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HystrixDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
